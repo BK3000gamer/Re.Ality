d7a76a34b055cb91f3bcacff7e7de849e88a3083
BK3000gamer <bk3000gamer@gmail.com>
2025-06-13 20:02:09 +0700

    more shader fixes

--- Levels/Environment/Walls/wall.gd
+++ Levels/Environment/Walls/wall.gd
@@ -3,19 +3,24 @@ class_name Wall
 
 var pivot: Pivot
 var player: Player
-
-func _process(delta: float) -> void:
+	
+func _process(delta: float) -> void:	
 	var PlayerPos = player.global_position
 	var CamPos = pivot.CamPos
 	var ObjPos = global_position
 	
 	if pivot.CurrentState == "SideNS":
 		if (PlayerPos.x < ObjPos.x and ObjPos.x < CamPos.x) or (CamPos.x < ObjPos.x and ObjPos.x < PlayerPos.x):
-			set_transparency(abs(PlayerPos.x - ObjPos.x) / 10)
+			update_transparency(abs(PlayerPos.x - ObjPos.x) / 10)
 		else:
-			set_transparency(0)
+			update_transparency(1)
 	elif pivot.CurrentState == "SideWE":
 		if (PlayerPos.z < ObjPos.z and ObjPos.z < CamPos.z) or (CamPos.z < ObjPos.z and ObjPos.z < PlayerPos.z):
-			set_transparency(abs(PlayerPos.z - ObjPos.z) / 10)
+			update_transparency(abs(PlayerPos.z - ObjPos.z) / 10)
 		else:
-			set_transparency(0)
+			update_transparency(1)
+
+func update_transparency(value: float) -> void:
+	var mat := material_override
+	if mat is ShaderMaterial:
+		mat.set_shader_parameter("transparency", clamp(value, 0.0, 1.0))
